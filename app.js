0a18dd905492dcf1a493300cbd59cd61f641c39a
Michael Comatas <mcomatas99@gmail.com>
2022-04-25 09:57:50 -0400

    Added a working dynamic background based on the time of day

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const switcher = document.querySelector('.btn');
+var currentTime = new Date();
 
-switcher.addEventListener('click', function() {
+/* switcher.addEventListener('click', function() {
     
     document.body.classList.toggle('dark-theme')
     var className = document.body.className;
@@ -17,4 +18,43 @@ switcher.addEventListener('click', function() {
     
     console.log( 'Current class name: ' + className );
     //document.body.classList.toggle('dark-theme')
-});
+}); */
+
+var hour = currentTime.getHours();
+/* console.log( hour );
+console.log( document.body.className );*/
+
+if ( hour >= 5 && hour < 8 )
+{
+    document.body.classList.toggle('dawn');
+}
+else if ( hour >= 8 && hour < 11 )
+{
+    document.body.classList.toggle('early-morning');
+}
+else if ( hour >= 11 && hour < 14 )
+{
+    document.body.classList.toggle('morning');
+}
+else if ( hour >= 14 && hour < 17 )
+{
+    document.body.classList.toggle('afternoon');
+}
+else if ( hour >= 17 && hour < 20 )
+{
+    document.body.classList.toggle('evening');
+}
+else if ( hour >= 20 && hour < 23 )
+{
+    document.body.classList.toggle('dusk');
+}
+else if ( hour >= 23 && hour < 2 )
+{
+    document.body.classList.toggle('midnight');
+}
+else if ( hour >= 2 && hour < 5 )
+{
+    document.body.classList.toggle('night');
+}
+
+console.log( document.body.className );
