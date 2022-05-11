c3badb4c347d0e513045ecce16062130bc7feaa6
Michael Comatas <mcomatas99@gmail.com>
2022-05-11 11:57:43 -0400

    Attempt #7, changed the way the background image is set

--- app.js
+++ app.js
@@ -6,38 +6,47 @@ var hour = currentTime.getHours();
 /* console.log( hour );
 console.log( document.body.className );*/
 
+// console.log( Date.now() );
+
+// console.log( document.querySelector('.midnight') );
+
 if ( hour >= 5 && hour < 8 )
 {
-    document.body.classList.toggle('dawn');
+    document.body.style.background = "url('./pixelDay/dawn.png')";
+    // document.body.classList.toggle('dawn');
 }
 else if ( hour >= 8 && hour < 11 )
 {
-    document.body.classList.toggle('early-morning');
+    document.body.style.background = "url('./pixelDay/early-morning.png')";
+    // document.body.classList.toggle('early-morning');
 }
 else if ( hour >= 11 && hour < 14 )
 {
-    document.body.classList.toggle('morning');
+    document.body.style.background = "url('./pixelDay/morning.png')";
+    // document.body.classList.toggle('morning');
 }
 else if ( hour >= 14 && hour < 17 )
 {
-    document.body.classList.toggle('afternoon');
+    document.body.style.background = "url('./pixelDay/afternoon.png')";
+    // document.body.classList.toggle('afternoon');
 }
 else if ( hour >= 17 && hour < 20 )
 {
-    document.body.classList.toggle('evening');
+    document.body.style.background = "url('./pixelDay/evening.png')";
+    // document.body.classList.toggle('evening');
 }
 else if ( hour >= 20 && hour < 23 )
 {
-    document.body.classList.toggle('dusk');
+    document.body.style.background = "url('./pixelDay/dusk.png')";
+    // document.body.classList.toggle('dusk');
 }
 else if ( hour >= 23 || hour < 2 )
 {
-    document.body.classList.toggle('midnight');
+    document.body.style.background = "url('./pixelDay/midnight.png')";
+    // document.body.classList.toggle('midnight');
 }
 else if ( hour >= 2 && hour < 5 )
 {
-    document.body.classList.toggle('night');
+    document.body.style.background = "url('./pixelDay/night.png')";
+    // document.body.classList.toggle('night');
 }
-
-// console.log( document.body.className );
-// console.log( hour );
